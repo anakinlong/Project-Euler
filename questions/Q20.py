e64daee6c2fb4d9aa8c0b771da574d621e1e05e8
anakin <alex@trevorder.com>
2023-09-17 05:50:23 +0100

    improve q20

--- questions/Q20.py
+++ questions/Q20.py
@@ -1,17 +1,37 @@
-'''Find the sum of the digits in the number 100!'''
+"""
+n! means n x (n - 1) x ... x 3 x 2 x 1.
 
-def factorial(n):
-    fact = 1
-    for i in range(1, n+1):
-        fact *= i
-    return fact
+For example, 10! = 10 x 9 x ... x 3 x 2 x 1 = 3628800,
+and the sum of the digits in the number 10! is 3 + 6 + 2 + 8 + 8 + 0 + 0 = 27.
 
-def sumOfDigits(n):
-    digits = str(n)
-    total = 0
-    for i in range(len(digits)):
-        total += int(digits[i])
-    print('Sum of digits = ' + str(total))
+Find the sum of the digits in the number 100!.
+"""
+
+import math
+
+try:
+    import lib
+except ModuleNotFoundError:
+    from questions import lib
+
+
+ANSWER = 648
+
+
+@lib.profiling.profileit()
+def sum_of_factorial_digits(n: int) -> int:
+    """
+    Find the sum of the digits of n! (n factorial).
+
+    :param n: an integer.
+
+    :return: the sum of the digits of n!.
+    """
+    n_factorial = math.factorial(n)
+
+    return lib.general.sum_of_digits(n_factorial)
+
 
 if __name__ == '__main__':
-    sumOfDigits(factorial(100))
+
+    answer = sum_of_factorial_digits(100)
