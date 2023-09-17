db410e2978b8b80e29fe82eca05bea36ac24e14f
anakin <alex@trevorder.com>
2023-09-17 03:27:40 +0100

    add a function to read a particular question

--- home.py
+++ home.py
@@ -6,6 +6,27 @@ import importlib
 import questions
 
 
+def question(question_number: int) -> str | None:
+    """
+    Return the wording of a particular question.
+
+    :param question_number: the integer question number.
+
+    :return: the __doc__ attribute from the corresponding question module. If it doesn't exist, then None.
+    """
+    # Attempt to find the module containing the code for this question:
+    module_name = f"Q{str(question_number)}"
+    if module_name not in questions.__all__:
+        raise ValueError(
+            f"Could not find associated module name '{module_name}' for question '{question_number}'."
+        )
+
+    # Attempt to find the __doc__ attribute in the given module:
+    module = importlib.import_module("." + module_name, "questions")
+
+    return getattr(module, "__doc__", None)
+
+
 def answer(question_number: int) -> int | None:
     """
     Return the answer to a particular question - if the answer doesn't exist, return None.
