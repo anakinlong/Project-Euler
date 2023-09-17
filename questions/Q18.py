0fb91b7afe1682e9147cafd2d2e78a1624e788ee
anakin <alex@trevorder.com>
2023-09-17 05:17:50 +0100

    improve q18

--- questions/Q18.py
+++ questions/Q18.py
@@ -1,7 +1,32 @@
-'''By starting at the top of the triangle below and moving to adjacent numbers on the row below,
-find the maximum total from top to bottom of the triangle below:'''
+"""
+By starting at the top of the triangle below and moving to adjacent numbers on the row below, the maximum total from top
+to bottom is 23.
 
-triangle = '''75
+3
+7 4
+2 4 6
+8 5 9 3
+
+That is, 3 + 7 + 4 + 9 = 23.
+
+Find the maximum total from top to bottom of the triangle below.
+
+Note: As there are only 16384 routes, it is possible to solve this problem by trying every route. However, Problem 67,
+is the same challenge with a triangle containing one-hundred rows; it cannot be solved by brute force, and requires a
+clever method! ;o)
+"""
+
+try:
+    import lib
+except ModuleNotFoundError:
+    from questions import lib
+
+
+ANSWER = 1074
+
+
+TRIANGLE = """
+75
 95 64
 17 47 82
 18 35 87 10
@@ -15,31 +40,40 @@ triangle = '''75
 70 11 33 28 77 73 17 78 39 68 17 57
 91 71 52 38 17 14 91 43 58 50 27 29 48
 63 66 04 68 89 53 67 30 73 16 69 87 40 31
-04 62 98 27 23 09 70 98 73 93 38 53 60 04 23'''
+04 62 98 27 23 09 70 98 73 93 38 53 60 04 23
+"""
 
-small_triangle = '''3
-7 4
-2 4 6
-8 5 9 3'''
 
-def triangle_to_array(triangle):
-    split_triangle = triangle.split('\n')
-    triangle_array = []
-    for i in range(len(split_triangle)):
-        new_row = list(map(int, split_triangle[i].split(' ')))
-        triangle_array.append(new_row)
-    return triangle_array
+@lib.profiling.profileit()
+def max_adjacent_sum(triangle: str) -> int:
+    """
+    Calculate the maximum total resulting from a path of adjacent numbers from the top of the triangle to the bottom.
 
-def max_adjacent_sum(triangle):
-    triangle_array = triangle_to_array(triangle)
-    current_sum = triangle_array[-1]
-    for i in range(len(triangle_array) - 2, -1, -1):
-        new_sum = triangle_array[i]
-        for j in range(len(new_sum)):
-            new_sum[j] += max(current_sum[j], current_sum[j+1])
-        current_sum = new_sum
-    return current_sum[0]
+    :param triangle: the triangle in string form.
+
+    :return: the sum of the values in the maximum path.
+    """
+    # Convert the triangle into a list of lists of ints, with each row being a list:
+    triangle_array = lib.input_processing.grid_to_array(triangle)
+    n_rows = len(triangle_array)
+
+    # Starting with the bottom row of the triangle:
+    current_row = triangle_array[-1]
+    # Loop through each row of the triangle from the second to last upwards:
+    for i in range(n_rows - 2, -1, -1):
+        row_above = triangle_array[i]
+        # For each number in the row above, we choose which path we would like to follow below it.
+        # The options are the optimal paths from each of the adjacent numbers below:
+        for j in range(len(row_above)):
+            row_above[j] += max(current_row[j], current_row[j + 1])
+        # Set the current row to the row above, ready for the next iteration:
+        current_row = row_above
+
+    # Once the loop is complete, the "current row" should just be the top row of the triangle with the maximum possible
+    # total:
+    return current_row[0]
+
 
 if __name__ == '__main__':
-    #print(triangle_to_array(triangle))
-    #print(max_adjacent_sum(triangle))
+
+    max_adjacent_sum(TRIANGLE)
