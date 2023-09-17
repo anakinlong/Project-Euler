fc81a00ac09d13cc89e1c123956e74a58a0b241a
anakin <alex@trevorder.com>
2023-09-17 21:36:46 +0100

    improve q25

--- questions/Q25.py
+++ questions/Q25.py
@@ -1,22 +1,55 @@
 '''What is the index of the first term in the Fibonacci sequence to contain 1000 digits?'''
 
-f1 = 1
-f2 = 1
+try:
+    import lib
+except ModuleNotFoundError:
+    from questions import lib
 
-def function(nDigits):
-    fnminus2 = f1
-    fnminus1 = f2
-    found = False
-    index = 3
-    while found == False:
-        fn = fnminus1 + fnminus2
-        if len(str(fn)) >= nDigits:
-            print(index)
-            found = True
-        else:
-            fnminus2 = fnminus1
-            fnminus1 = fn
-            index += 1
+
+ANSWER = 4782
+
+
+def number_of_digits(n: int) -> int:
+    """
+    Return the number of digits n has.
+
+    :param n: an integer.
+
+    :return: the number of digits.
+    """
+    return len(str(n))
+
+
+@lib.profiling.profileit()
+def find_first_n_digit_fibonacci(n_digits: int, a_1: int, a_2: int) -> int:
+    """
+    Calculate first Fibonacci number to contain at least n_digits digits.
+
+    :param n_digits: how many digits we are aiming for.
+    :param a_1: the first term of the sequence.
+    :param a_2: the second term of the sequence.
+
+    :return: the first Fibonacci number with the specified number of  digits.
+    """
+    # Check whether the first or second term meets the required number of digits:
+    for index, term in {1: a_1, 2: a_2}.items():
+        if number_of_digits(term) >= n_digits:
+            return index
+
+    # If not, then we will have to calculate subsequent terms and test whether each has the required number of digits:
+    previous_term = a_1
+    current_term = a_2
+    index = 2
+    while True:
+        # Calculate the next term:
+        index += 1
+        current_term, previous_term = current_term + previous_term, current_term
+
+        if number_of_digits(current_term) >= n_digits:
+
+            return index
+
 
 if __name__ == '__main__':
-    function(1000)
+
+    answer = find_first_n_digit_fibonacci(1000, 1, 1)
