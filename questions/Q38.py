85e1bde1d655618c1c24c7c7753cbb97ca993c8c
anakin <alex@trevorder.com>
2023-09-24 20:26:44 +0100

    improve q38

--- questions/Q38.py
+++ questions/Q38.py
@@ -1,36 +1,58 @@
-'''What is the largest 1 to 9 pandigital 9-digit number that can be formed as the concatenated product 
-of an integer with (1,2, ... , n) where n > 1?'''
+"""
+Take the number 192 and multiply it by each of 1, 2, and 3:
 
-import numpy
+192 x 1 = 192
+192 x 2 = 384
+192 x 3 = 576
 
-def is_pandigital(n, n_digits):
-    if len(str(n)) == n_digits:
-        check = numpy.zeros(n_digits)
-        for digit in str(n):
-            if int(digit) != 0:
-                check[int(digit) - 1] += 1
-        if all(check == numpy.ones(n_digits)):
-            return True
-        else:
-            return False
-    else:
-        return False
+By concatenating each product we get the 1 to 9 pandigital, 192384576.
 
-def find(max_excl=10000):
+The same can be achieved by starting with 9 and multiplying by 1, 2, 3, 4, and 5, giving the pandigital, 918273645,
+which is the concatenated product of 9 and (1, 2, 3, 4, 5).
+
+What is the largest 1 to 9 pandigital 9-digit number that can be formed as the concatenated product of an integer with
+(1,2, ... , n) where n > 1?
+"""
+
+try:
+    import lib
+except ModuleNotFoundError:
+    from questions import lib
+
+
+ANSWER = 932718654
+
+
+@lib.profiling.profileit()
+def find_maximum_concatenated_products() -> int:
+    """
+    Find the largest 1 to 9 pandigital 9-digit number that can be formed as the concatenated product of an integer with
+    (1,2, ... , n) where n > 1.
+
+    :return: the largest value found.
+    """
     best = 0
-    for n in range(1, max_excl):
-        for i in range(3, 10):
-            products = [n * x for x in range(1, i)]
-            max_concatenated = ''
-            for number in products:
-                max_concatenated += str(number)
-            if is_pandigital(int(max_concatenated), 9) == True:
-                if int(max_concatenated) > best:
-                    best = int(max_concatenated)
+    # Since we need to at least take 2 products and concatenate them, the number is at most 4 digits long:
+    for n in range(1, int(1e4)):
+        # Calculate all multiples, then concatenate until the result is 9 digits or longer:
+        products = [str(n * x) for x in range(1, 10)]
+        concatenated_products = ""
+        n_digits = 0
+        i = 0
+        while n_digits < 9:
+            new_digits = products[i]
+            concatenated_products += new_digits
+            n_digits += len(new_digits)
+            i += 1
+        # If the result is 9 digits long, larger than the current best, and pandigital, set it as our new largest:
+        if n_digits == 9:
+            new_value = int(concatenated_products)
+            if new_value > best and lib.pandigital.is_pandigital(new_value):
+                best = new_value
+
     return best
 
 
 if __name__ == '__main__':
-    #print(is_pandigital(43120567, 8))
-    #print(sorted([10 * x for x in range(1, 20)], key=str)[::-1])
-    print(find(10000))
+
+    answer = find_maximum_concatenated_products()
--- questions/lib/pandigital.py
+++ questions/lib/pandigital.py
@@ -19,17 +19,22 @@ def is_pandigital(n: int) -> bool:
     # First separate n into its digits:
     n_as_digits = [int(digit) for digit in str(n)]
     n_digits = len(n_as_digits)
+    acceptable_digits = range(1, n_digits + 1)
 
     # Then count how many of each there are:
-    digit_count = {k: 0 for k in range(1, n_digits + 1)}
+    digit_count = {k: 0 for k in acceptable_digits}
 
     for digit in n_as_digits:
         # If the digit is zero, then we are done:
         if digit == 0:
             return False
-        # Otherwise, add one to this digit's count:
+        # Otherwise, attempt to add one to this digit's count:
         else:
-            digit_count[digit] += 1
+            if digit in acceptable_digits:
+                digit_count[digit] += 1
+            # If this digit is not in the range [1, n_digits], the number can't be pandigital anyway:
+            else:
+                return False
 
     # If all digits have a count of 1, then n is pandigital:
     if all(count == 1 for count in digit_count.values()):
